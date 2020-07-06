7d4870aee3a744fdaa50e9e14d4463caf3ad9be6
Ashlee Barksdale <ashleelennab@gmail.com>
2020-07-06 23:06:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,13 +17,13 @@ def load_library(file_name)
 end
 
 def get_japanese_emoticon(emote)
+ library = load_library(file_path)
  
 end
 
 
 def get_english_meaning
-  # code goes here
-  
+  library = load_library(file_path)
   binding.pry
 end
 
